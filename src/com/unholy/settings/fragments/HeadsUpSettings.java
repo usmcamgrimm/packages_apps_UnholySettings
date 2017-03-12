knmurthy/android_packages_apps_PureSettings

Add a HeadsUp customization fragment with switchbar

src/com/pure/settings/fragments/HeadsUpSettings.java
@@ -1,0 +1,126 @@
+/*
+ * Copyright (C) 2016 The Pure Nexus Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pure.settings.fragments;
+
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+
+import com.android.settings.R;
+import com.android.settings.SettingsActivity;
+import com.android.settings.SettingsPreferenceFragment;
+import com.pure.settings.utils.Utils;
+
+import com.pure.settings.preferences.BaseGlobalSettingSwitchBar;
+
+public class HeadsUpSettings extends SettingsPreferenceFragment
+        implements BaseGlobalSettingSwitchBar.SwitchBarChangeCallback {
+
+    private BaseGlobalSettingSwitchBar mEnabledSwitch;
+
+    private ViewGroup mPrefsContainer;
+    private View mDisabledText;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Get launch-able applications
+        addPreferencesFromResource(R.xml.headsup_settings);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.headsup_fragment, container, false);
+        mPrefsContainer = (ViewGroup) v.findViewById(R.id.prefs_container);
+        mDisabledText = v.findViewById(R.id.disabled_text);
+
+        View prefs = super.onCreateView(inflater, mPrefsContainer, savedInstanceState);
+        mPrefsContainer.addView(prefs);
+
+        return v;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        final SettingsActivity activity = (SettingsActivity) getActivity();
+        mEnabledSwitch = new BaseGlobalSettingSwitchBar(activity, activity.getSwitchBar(),
+                Settings.Global.HEADS_UP_NOTIFICATIONS_ENABLED, true, this);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        final SettingsActivity activity = (SettingsActivity) getActivity();
+        if (mEnabledSwitch != null) {
+            mEnabledSwitch.resume(activity);
+        }
+
+        if (!Utils.isTablet(getActivity())) {
+            mPrefsContainer.setPadding(0, 0, 0, 0);
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mEnabledSwitch != null) {
+            mEnabledSwitch.pause();
+        }
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        if (mEnabledSwitch != null) {
+            mEnabledSwitch.teardownSwitchBar();
+        }
+    }
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsEvent.PURE;
+    }
+
+    private boolean getUserHeadsUpState() {
+         return Settings.Global.getInt(getContentResolver(),
+                Settings.Global.HEADS_UP_NOTIFICATIONS_ENABLED,
+                Settings.Global.HEADS_UP_ON) != 0;
+    }
+
+    private void setUserHeadsUpState(int val) {
+         Settings.Global.putInt(getContentResolver(),
+                Settings.Global.HEADS_UP_NOTIFICATIONS_ENABLED, val);
+    }
+
+    private void updateEnabledState() {
+        mPrefsContainer.setVisibility(getUserHeadsUpState() ? View.VISIBLE : View.GONE);
+        mDisabledText.setVisibility(getUserHeadsUpState() ? View.GONE : View.VISIBLE);
+    }
+
+    @Override
+    public void onEnablerChanged(boolean isEnabled) {
+        setUserHeadsUpState(getUserHeadsUpState() ? 1 : 0);
+        updateEnabledState();
+    }
+}

src/com/pure/settings/preferences/BaseGlobalSettingSwitchBar.java
@@ -1,0 +1,155 @@
+/*
+* Copyright (C) 2014 The CyanogenMod Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.pure.settings.preferences;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.Handler;
+import android.provider.Settings;
+import android.widget.Switch;
+import com.android.settings.widget.SwitchBar;
+
+public class BaseGlobalSettingSwitchBar implements SwitchBar.OnSwitchChangeListener  {
+    private Context mContext;
+    private SwitchBar mSwitchBar;
+    private SettingsObserver mSettingsObserver;
+    private boolean mListeningToOnSwitchChange = false;
+
+    private boolean mStateMachineEvent;
+
+    private final String mSettingKey;
+    private final int mDefaultState;
+
+    private final SwitchBarChangeCallback mCallback;
+    public interface SwitchBarChangeCallback {
+        public void onEnablerChanged(boolean isEnabled);
+    }
+
+    public BaseGlobalSettingSwitchBar(Context context, SwitchBar switchBar, String key,
+                                      boolean defaultState, SwitchBarChangeCallback callback) {
+        mContext = context;
+        mSwitchBar = switchBar;
+        mSettingKey = key;
+        mDefaultState = defaultState ? 1 : 0;
+        mCallback = callback;
+        mSettingsObserver = new SettingsObserver(new Handler());
+        setupSwitchBar();
+    }
+
+    public void setupSwitchBar() {
+        setSwitchState();
+        if (!mListeningToOnSwitchChange) {
+            mSwitchBar.addOnSwitchChangeListener(this);
+            mListeningToOnSwitchChange = true;
+        }
+        mSwitchBar.show();
+    }
+
+    public void teardownSwitchBar() {
+        if (mListeningToOnSwitchChange) {
+            mSwitchBar.removeOnSwitchChangeListener(this);
+            mListeningToOnSwitchChange = false;
+        }
+        mSwitchBar.hide();
+    }
+
+    public void resume(Context context) {
+        mContext = context;
+        if (!mListeningToOnSwitchChange) {
+            mSwitchBar.addOnSwitchChangeListener(this);
+            mSettingsObserver.observe();
+
+            mListeningToOnSwitchChange = true;
+        }
+    }
+
+    public void pause() {
+        if (mListeningToOnSwitchChange) {
+            mSwitchBar.removeOnSwitchChangeListener(this);
+            mSettingsObserver.unobserve();
+
+            mListeningToOnSwitchChange = false;
+        }
+    }
+
+    private void setSwitchBarChecked(boolean checked) {
+        mStateMachineEvent = true;
+        mSwitchBar.setChecked(checked);
+        mStateMachineEvent = false;
+        if (mCallback != null) {
+            mCallback.onEnablerChanged(checked);
+        }
+    }
+
+    private void setSwitchState() {
+        boolean enabled = Settings.Global.getInt(mContext.getContentResolver(),
+                mSettingKey, mDefaultState) == 1;
+        mStateMachineEvent = true;
+        setSwitchBarChecked(enabled);
+        mStateMachineEvent = false;
+    }
+
+    @Override
+    public void onSwitchChanged(Switch switchView, boolean isChecked) {
+        //Do nothing if called as a result of a state machine event
+        if (mStateMachineEvent) {
+            return;
+        }
+
+        // Handle a switch change
+        Settings.Global.putInt(mContext.getContentResolver(),
+                mSettingKey, isChecked ? 1 : 0);
+
+        if (mCallback != null) {
+            mCallback.onEnablerChanged(isChecked);
+        }
+    }
+
+    class SettingsObserver extends ContentObserver {
+        SettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(Settings.Global.getUriFor(
+                    mSettingKey), false, this);
+            update();
+        }
+
+        void unobserve() {
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.unregisterContentObserver(this);
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            update();
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            update();
+        }
+
+        public void update() {
+            setSwitchState();
+        }
+    }
+}

usmcamgrimm/packages_apps_UnholySettings

src/com/unholy/settings/fragments/HeadsUpSettings.java
@@ -1,0 +1,126 @@
+/*
+ * Copyright (C) 2017 The Pure Nexus Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.unholy.settings.fragments;
+
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+
+import com.android.settings.R;
+import com.android.settings.SettingsActivity;
+import com.android.settings.SettingsPreferenceFragment;
+import com.unholy.settings.utils.Utils;
+
+import com.unholy.settings.preference.BaseGlobalSettingSwitchBar;
+
+public class HeadsUpSettings extends SettingsPreferenceFragment
+        implements BaseGlobalSettingSwitchBar.SwitchBarChangeCallback {
+
+    private BaseGlobalSettingSwitchBar mEnabledSwitch;
+
+    private ViewGroup mPrefsContainer;
+    private View mDisabledText;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Get launch-able applications
+        addPreferencesFromResource(R.xml.headsup_settings);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.headsup_fragment, container, false);
+        mPrefsContainer = (ViewGroup) v.findViewById(R.id.prefs_container);
+        mDisabledText = v.findViewById(R.id.disabled_text);
+
+        View prefs = super.onCreateView(inflater, mPrefsContainer, savedInstanceState);
+        mPrefsContainer.addView(prefs);
+
+        return v;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        final SettingsActivity activity = (SettingsActivity) getActivity();
+        mEnabledSwitch = new BaseGlobalSettingSwitchBar(activity, activity.getSwitchBar(),
+                Settings.Global.HEADS_UP_NOTIFICATIONS_ENABLED, true, this);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        final SettingsActivity activity = (SettingsActivity) getActivity();
+        if (mEnabledSwitch != null) {
+            mEnabledSwitch.resume(activity);
+        }
+
+        if (!Utils.isTablet(getActivity())) {
+            mPrefsContainer.setPadding(0, 0, 0, 0);
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mEnabledSwitch != null) {
+            mEnabledSwitch.pause();
+        }
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        if (mEnabledSwitch != null) {
+            mEnabledSwitch.teardownSwitchBar();
+        }
+    }
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsEvent.UNHOLY_SETTINGS;
+    }
+
+    private boolean getUserHeadsUpState() {
+         return Settings.Global.getInt(getContentResolver(),
+                Settings.Global.HEADS_UP_NOTIFICATIONS_ENABLED,
+                Settings.Global.HEADS_UP_ON) != 0;
+    }
+
+    private void setUserHeadsUpState(int val) {
+         Settings.Global.putInt(getContentResolver(),
+                Settings.Global.HEADS_UP_NOTIFICATIONS_ENABLED, val);
+    }
+
+    private void updateEnabledState() {
+        mPrefsContainer.setVisibility(getUserHeadsUpState() ? View.VISIBLE : View.GONE);
+        mDisabledText.setVisibility(getUserHeadsUpState() ? View.GONE : View.VISIBLE);
+    }
+
+    @Override
+    public void onEnablerChanged(boolean isEnabled) {
+        setUserHeadsUpState(getUserHeadsUpState() ? 1 : 0);
+        updateEnabledState();
+    }
+}

src/com/unholy/settings/preference/BaseGlobalSettingSwitchBar.java
@@ -1,0 +1,155 @@
+/*
+* Copyright (C) 2014 The CyanogenMod Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.unholy.settings.preference;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.Handler;
+import android.provider.Settings;
+import android.widget.Switch;
+import com.android.settings.widget.SwitchBar;
+
+public class BaseGlobalSettingSwitchBar implements SwitchBar.OnSwitchChangeListener  {
+    private Context mContext;
+    private SwitchBar mSwitchBar;
+    private SettingsObserver mSettingsObserver;
+    private boolean mListeningToOnSwitchChange = false;
+
+    private boolean mStateMachineEvent;
+
+    private final String mSettingKey;
+    private final int mDefaultState;
+
+    private final SwitchBarChangeCallback mCallback;
+    public interface SwitchBarChangeCallback {
+        public void onEnablerChanged(boolean isEnabled);
+    }
+
+    public BaseGlobalSettingSwitchBar(Context context, SwitchBar switchBar, String key,
+                                      boolean defaultState, SwitchBarChangeCallback callback) {
+        mContext = context;
+        mSwitchBar = switchBar;
+        mSettingKey = key;
+        mDefaultState = defaultState ? 1 : 0;
+        mCallback = callback;
+        mSettingsObserver = new SettingsObserver(new Handler());
+        setupSwitchBar();
+    }
+
+    public void setupSwitchBar() {
+        setSwitchState();
+        if (!mListeningToOnSwitchChange) {
+            mSwitchBar.addOnSwitchChangeListener(this);
+            mListeningToOnSwitchChange = true;
+        }
+        mSwitchBar.show();
+    }
+
+    public void teardownSwitchBar() {
+        if (mListeningToOnSwitchChange) {
+            mSwitchBar.removeOnSwitchChangeListener(this);
+            mListeningToOnSwitchChange = false;
+        }
+        mSwitchBar.hide();
+    }
+
+    public void resume(Context context) {
+        mContext = context;
+        if (!mListeningToOnSwitchChange) {
+            mSwitchBar.addOnSwitchChangeListener(this);
+            mSettingsObserver.observe();
+
+            mListeningToOnSwitchChange = true;
+        }
+    }
+
+    public void pause() {
+        if (mListeningToOnSwitchChange) {
+            mSwitchBar.removeOnSwitchChangeListener(this);
+            mSettingsObserver.unobserve();
+
+            mListeningToOnSwitchChange = false;
+        }
+    }
+
+    private void setSwitchBarChecked(boolean checked) {
+        mStateMachineEvent = true;
+        mSwitchBar.setChecked(checked);
+        mStateMachineEvent = false;
+        if (mCallback != null) {
+            mCallback.onEnablerChanged(checked);
+        }
+    }
+
+    private void setSwitchState() {
+        boolean enabled = Settings.Global.getInt(mContext.getContentResolver(),
+                mSettingKey, mDefaultState) == 1;
+        mStateMachineEvent = true;
+        setSwitchBarChecked(enabled);
+        mStateMachineEvent = false;
+    }
+
+    @Override
+    public void onSwitchChanged(Switch switchView, boolean isChecked) {
+        //Do nothing if called as a result of a state machine event
+        if (mStateMachineEvent) {
+            return;
+        }
+
+        // Handle a switch change
+        Settings.Global.putInt(mContext.getContentResolver(),
+                mSettingKey, isChecked ? 1 : 0);
+
+        if (mCallback != null) {
+            mCallback.onEnablerChanged(isChecked);
+        }
+    }
+
+    class SettingsObserver extends ContentObserver {
+        SettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(Settings.Global.getUriFor(
+                    mSettingKey), false, this);
+            update();
+        }
+
+        void unobserve() {
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.unregisterContentObserver(this);
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            update();
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            update();
+        }
+
+        public void update() {
+            setSwitchState();
+        }
+    }
+}