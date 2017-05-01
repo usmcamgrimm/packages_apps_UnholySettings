sc4ryb3ar/android_packages_apps_PureSettings

Unlock keystore with fingerprint after reboot (2/2)

src/com/pure/settings/fragments/LockScreenSettings.java
@@ -20,6 +20,7 @@ import android.content.Context;
 import android.hardware.fingerprint.FingerprintManager;
 import android.os.Bundle;
 import android.os.UserHandle;
+import android.support.v14.preference.SwitchPreference;
 import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceScreen;
 
@@ -40,11 +41,13 @@ public class LockScreenSettings extends SettingsPreferenceFragment {
 
     private static final String KEYGUARD_TORCH = "keyguard_toggle_torch";
     private static final String FINGERPRINT_VIB = "fingerprint_success_vib";
+    private static final String FP_UNLOCK_KEYSTORE = "fp_unlock_keystore";
 
     private FingerprintManager mFingerprintManager;
 
     private SystemSettingSwitchPreference mLsTorch;
     private SystemSettingSwitchPreference mFingerprintVib;
+    private SwitchPreference mFpKeystore;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -63,8 +66,10 @@ public class LockScreenSettings extends SettingsPreferenceFragment {
 
         mFingerprintManager = (FingerprintManager) getActivity().getSystemService(Context.FINGERPRINT_SERVICE);
         mFingerprintVib = (SystemSettingSwitchPreference) findPreference(FINGERPRINT_VIB);
+        mFpKeystore = (SwitchPreference) findPreference(FP_UNLOCK_KEYSTORE);
         if (!mFingerprintManager.isHardwareDetected()){
             secureCategory.removePreference(mFingerprintVib);
+            secureCategory.removePreference(mFpKeystore);
         }
 
         if (!lockPatternUtils.isSecure(MY_USER_ID)) {

usmcamgrimm/packages_apps_UnholySettings

src/com/unholy/settings/fragments/LockScreenSettings.java
@@ -27,13 +27,19 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.unholy.settings.utils.Utils;
 import com.unholy.settings.preference.SystemSettingSwitchPreference;
 
+import android.hardware.fingerprint.FingerprintManager;
+
 public class LockScreenSettings extends SettingsPreferenceFragment {
 
     private static final int MY_USER_ID = UserHandle.myUserId();
 
     private static final String KEYGUARD_TORCH = "keyguard_toggle_torch";
+    private static final String FP_UNLOCK_KEYSTORE = "fp_unlock_keystore";
+
+    private FingerprintManager mFingerprintManager;
 
     private SystemSettingSwitchPreference mLsTorch;
+    private SystemSettingSwitchPreference mFpKeystore;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -45,6 +51,12 @@ public class LockScreenSettings extends SettingsPreferenceFragment {
         if (!Utils.deviceSupportsFlashLight(getActivity())) {
             prefScreen.removePreference(mLsTorch);
         }
+
+        mFingerprintManager = (FingerprintManager) getActivity().getSystemService(Context.FINGERPRINT_SERVICE);
+        mFpKeystore = (SystemSettingSwitchPreference) findPreference(FP_UNLOCK_KEYSTORE);
+        if (!mFingerprintManager.isHardwareDetected()){
+            prefScreen.removePreference(mFpKeystore);
+        }
     }
 
     @Override
@@ -52,4 +64,3 @@ public class LockScreenSettings extends SettingsPreferenceFragment {
         return MetricsEvent.UNHOLY_SETTINGS;
     }
 }
-